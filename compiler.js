357011b3170712b060a592d04e2fa473dfbceaa0
Michael Shilman <michael@lab80.co>
2022-02-22 23:44:02 +0800

    Add skipCsf option

--- compiler.js
+++ compiler.js
@@ -1,6 +1,6 @@
 const { plugin, postprocess } = require('./dist/cjs');
 
-const compileAsync = async (code, options) => {
+const compileAsync = async (code, { skipCsf }) => {
   // This async import is needed because these libraries are ESM
   // and this file is CJS. Furthermore, we keep this file out of
   // the src directory so that babel doesn't turn these into `require`
@@ -9,12 +9,12 @@ const compileAsync = async (code, options) => {
   const { toEstree } = await import('hast-util-to-estree');
 
   const store = { exports: '', toEstree };
+  const rehypePlugins = skipCsf ? undefined : [[plugin, store]];
   const output = await compile(code, {
-    rehypePlugins: [[plugin, store]],
+    rehypePlugins,
     providerImportSource: '@mdx-js/react',
   });
-  const processed = postprocess(output.toString(), store.exports);
-  return processed;
+  return skipCsf ? output : postprocess(output, store.exports);
 };
 
 module.exports = { compile: compileAsync };
