53955e713c6f92585e126c0fdfbb8bb1d58ec176
Michael Shilman <shilman@users.noreply.github.com>
2022-11-04 11:13:42 +0800

    Upgrade example to SB7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', { name: '@storybook/addon-essentials' }],
-  framework: '@storybook/react',
-  core: { builder: 'webpack5' },
+  framework: '@storybook/react-webpack5',
   webpackFinal: async (config) => {
     const rules = (config.module.rules || []).filter(
       (rule) => !rule.test?.toString().endsWith('\\.mdx$/')
