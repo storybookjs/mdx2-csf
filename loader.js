b47ca0a9a27566de3d7ab37c8012066eb3df2abd
Michael Shilman <shilman@users.noreply.github.com>
2022-12-01 20:56:45 +0800

    Revert react removal

--- loader.js
+++ loader.js
@@ -1,6 +1,18 @@
 const { getOptions } = require('loader-utils');
 const { compile } = require('./dist/index');
 
+// FIXME: we shouldn't be doing this, but we need it
+// for react MDX story definitions, e.g.
+//
+// <Story name="foo"><div>hi></div></Story>
+//
+// Which generates the code:
+//
+// export const foo = () => <div>hi</div>;
+const DEFAULT_RENDERER = `
+import React from 'react';
+`;
+
 // Lifted from MDXv1 loader
 // https://github.com/mdx-js/mdx/blob/v1/packages/loader/index.js
 //
@@ -22,7 +34,8 @@ const loader = async function (content) {
     return callback(err);
   }
 
-  return callback(null, result);
+  const code = `${DEFAULT_RENDERER}\n${result}`;
+  return callback(null, code);
 };
 
 module.exports = loader;
